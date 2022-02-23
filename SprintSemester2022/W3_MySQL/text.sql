364664ee32acd41cb2cc6ca92bda3391fb4683d8
Ankit Kumar Verma <47997907+ankitverma5859@users.noreply.github.com>
2022-02-23 22:45:21 +0530

    Create text.sql

---
+++ SprintSemester2022/W3_MySQL/text.sql
@@ -0,0 +1,22 @@
+show tables;
+
+SELECT * 
+FROM CountryCodes;
+
+SELECT *
+FROM Matches;
+
+SELECT *
+FROM MatchResults;
+
+SELECt * 
+FROM PlayedIn;
+
+SELECT *
+FROM Player;
+
+SELECT * 
+FROM Registration;
+
+SELECT *
+FROM RetiredMatch;
