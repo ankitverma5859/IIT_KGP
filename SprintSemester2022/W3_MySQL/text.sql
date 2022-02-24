22da1ec303f092784fc0b2fb9d26cc62ddccc1b8
Ankit Kumar Verma <47997907+ankitverma5859@users.noreply.github.com>
2022-02-24 05:46:33 +0530

    Update text.sql

--- SprintSemester2022/W3_MySQL/text.sql
+++ SprintSemester2022/W3_MySQL/text.sql
@@ -20,3 +20,62 @@ FROM Registration;
 
 SELECT *
 FROM RetiredMatch;
+
+SELECT * 
+FROM SetScore;
+
+SELECT * FROM
+Tiebreaker;
+
+SELECT *
+FROM Tournament;
+
+SELECT TID, Name, Surface
+FROM Tournament;
+
+SELECT 
+TID AS "Tournament ID", Name as "Tournamenent Name", Surface as "Floor Type"
+FROM Tournament;
+
+SELECT CONCAT(StartDate, ' to ', EndDate) AS "Tournament Date"
+FROM Tournament;
+
+SELECT * 
+FROM Tournament
+ORDER BY NumRounds DESC;
+
+SELECT * 
+FROM Tournament
+ORDER BY NumRounds ASC;
+
+SELECT *
+FROM Tournament
+ORDER BY 
+NumRounds ASC,
+StartDate DESC;
+
+/*
+	Using the length of a columns as a SORT expression
+*/
+SELECT 
+	Name,
+    LENGTH(Name) as len
+FROM Tournament
+ORDER BY len ASC;
+
+/*
+	Selecting Columns number as a sort parameter.
+*/
+SELECT 
+	NumRounds,
+    StartDate
+FROM Tournament
+ORDER BY
+	1 ASC,
+    2 DESC;
+    
+/*
+	Selecting distinct values of a column
+*/    
+SELECT DISTINCT Location
+FROM Tournament;
